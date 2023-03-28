76b727f184998d8e42b04c2a26ae62b374338b4a
laynewin <127220244+laynewin@users.noreply.github.com>
2023-03-28 09:28:27 -0400

    Update main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,16 +1,30 @@
 input.onButtonPressed(Button.A, function () {
     servos.P0.setRange(70, 110)
-    servos.P0.run(44)
+    servos.P0.run(29)
     basic.pause(1000)
     servos.P0.stop()
     basic.pause(1000)
-    servos.P0.run(-77)
+    servos.P0.run(-60)
     basic.pause(1000)
     servos.P0.stop()
 })
-basic.forever(function () {
-	
+input.onButtonPressed(Button.B, function () {
+    servos.P1.setRange(70, 110)
+    servos.P1.run(26)
+    basic.pause(1000)
+    servos.P1.stop()
+    basic.pause(5000)
+    basic.pause(2000)
+    servos.P1.run(-52)
+    basic.pause(1000)
+    servos.P1.stop()
 })
 basic.forever(function () {
-    basic.showString("layne")
+    images.createBigImage(`
+        . . . . # . . . . .
+        . . . # . # . . . .
+        . . # . . . # . . .
+        . # . . . . . # . .
+        # . . . . . . . # .
+        `).scrollImage(1, 90)
 })
